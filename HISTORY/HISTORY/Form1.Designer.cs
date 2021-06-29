c657b518205561d10696f0bbe2edfb76f9b1a5ab
arya-1302194093 <79740589+arya-1302194093@users.noreply.github.com>
2021-06-28 22:22:11 -0700

    Desain Automata History

---
+++ HISTORY/HISTORY/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿
+namespace HISTORY
+{
+    partial class History_harian
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Senin = new System.Windows.Forms.Button();
+            this.Selasa = new System.Windows.Forms.Button();
+            this.rabu = new System.Windows.Forms.Button();
+            this.Kamis = new System.Windows.Forms.Button();
+            this.Jumaat = new System.Windows.Forms.Button();
+            this.Sabtu = new System.Windows.Forms.Button();
+            this.keluar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Box4 = new System.Windows.Forms.TextBox();
+            this.ts4 = new System.Windows.Forms.TextBox();
+            this.jam4 = new System.Windows.Forms.TextBox();
+            this.jam3 = new System.Windows.Forms.TextBox();
+            this.jam2 = new System.Windows.Forms.TextBox();
+            this.jam1 = new System.Windows.Forms.TextBox();
+            this.ts3 = new System.Windows.Forms.TextBox();
+            this.ts2 = new System.Windows.Forms.TextBox();
+            this.ts1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Box3 = new System.Windows.Forms.TextBox();
+            this.Box2 = new System.Windows.Forms.TextBox();
+            this.Box1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Senin
+            // 
+            this.Senin.Location = new System.Drawing.Point(169, 90);
+            this.Senin.Name = "Senin";
+            this.Senin.Size = new System.Drawing.Size(94, 29);
+            this.Senin.TabIndex = 0;
+            this.Senin.Text = "Senin";
+            this.Senin.UseVisualStyleBackColor = true;
+            this.Senin.Click += new System.EventHandler(this.Senin_Click);
+            // 
+            // Selasa
+            // 
+            this.Selasa.Location = new System.Drawing.Point(289, 90);
+            this.Selasa.Name = "Selasa";
+            this.Selasa.Size = new System.Drawing.Size(94, 29);
+            this.Selasa.TabIndex = 1;
+            this.Selasa.Text = "Selasa";
+            this.Selasa.UseVisualStyleBackColor = true;
+            this.Selasa.Click += new System.EventHandler(this.Selasa_Click);
+            // 
+            // rabu
+            // 
+            this.rabu.Location = new System.Drawing.Point(413, 90);
+            this.rabu.Name = "rabu";
+            this.rabu.Size = new System.Drawing.Size(94, 29);
+            this.rabu.TabIndex = 2;
+            this.rabu.Text = "Rabu";
+            this.rabu.UseVisualStyleBackColor = true;
+            this.rabu.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Kamis
+            // 
+            this.Kamis.Location = new System.Drawing.Point(169, 150);
+            this.Kamis.Name = "Kamis";
+            this.Kamis.Size = new System.Drawing.Size(94, 29);
+            this.Kamis.TabIndex = 3;
+            this.Kamis.Text = "Kamis";
+            this.Kamis.UseVisualStyleBackColor = true;
+            this.Kamis.Click += new System.EventHandler(this.Kamis_Click);
+            // 
+            // Jumaat
+            // 
+            this.Jumaat.Location = new System.Drawing.Point(289, 150);
+            this.Jumaat.Name = "Jumaat";
+            this.Jumaat.Size = new System.Drawing.Size(94, 29);
+            this.Jumaat.TabIndex = 4;
+            this.Jumaat.Text = "Jumaat";
+            this.Jumaat.UseVisualStyleBackColor = true;
+            this.Jumaat.Click += new System.EventHandler(this.Jumaat_Click);
+            // 
+            // Sabtu
+            // 
+            this.Sabtu.Location = new System.Drawing.Point(413, 150);
+            this.Sabtu.Name = "Sabtu";
+            this.Sabtu.Size = new System.Drawing.Size(94, 29);
+            this.Sabtu.TabIndex = 5;
+            this.Sabtu.Text = "Sabtu";
+            this.Sabtu.UseVisualStyleBackColor = true;
+            this.Sabtu.Click += new System.EventHandler(this.Sabtu_Click);
+            // 
+            // keluar
+            // 
+            this.keluar.Location = new System.Drawing.Point(289, 495);
+            this.keluar.Name = "keluar";
+            this.keluar.Size = new System.Drawing.Size(94, 36);
+            this.keluar.TabIndex = 6;
+            this.keluar.Text = "Keluar";
+            this.keluar.UseVisualStyleBackColor = true;
+            this.keluar.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Box4);
+            this.groupBox1.Controls.Add(this.ts4);
+            this.groupBox1.Controls.Add(this.jam4);
+            this.groupBox1.Controls.Add(this.jam3);
+            this.groupBox1.Controls.Add(this.jam2);
+            this.groupBox1.Controls.Add(this.jam1);
+            this.groupBox1.Controls.Add(this.ts3);
+            this.groupBox1.Controls.Add(this.ts2);
+            this.groupBox1.Controls.Add(this.ts1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.Box3);
+            this.groupBox1.Controls.Add(this.Box2);
+            this.groupBox1.Controls.Add(this.Box1);
+            this.groupBox1.Location = new System.Drawing.Point(51, 185);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(614, 304);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "View";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Box4
+            // 
+            this.Box4.Location = new System.Drawing.Point(11, 247);
+            this.Box4.Name = "Box4";
+            this.Box4.Size = new System.Drawing.Size(161, 27);
+            this.Box4.TabIndex = 15;
+            // 
+            // ts4
+            // 
+            this.ts4.Location = new System.Drawing.Point(221, 247);
+            this.ts4.Name = "ts4";
+            this.ts4.Size = new System.Drawing.Size(135, 27);
+            this.ts4.TabIndex = 14;
+            // 
+            // jam4
+            // 
+            this.jam4.Location = new System.Drawing.Point(396, 247);
+            this.jam4.Name = "jam4";
+            this.jam4.Size = new System.Drawing.Size(125, 27);
+            this.jam4.TabIndex = 14;
+            // 
+            // jam3
+            // 
+            this.jam3.Location = new System.Drawing.Point(396, 202);
+            this.jam3.Name = "jam3";
+            this.jam3.Size = new System.Drawing.Size(125, 27);
+            this.jam3.TabIndex = 13;
+            // 
+            // jam2
+            // 
+            this.jam2.Location = new System.Drawing.Point(396, 151);
+            this.jam2.Name = "jam2";
+            this.jam2.Size = new System.Drawing.Size(125, 27);
+            this.jam2.TabIndex = 12;
+            // 
+            // jam1
+            // 
+            this.jam1.Location = new System.Drawing.Point(396, 88);
+            this.jam1.Name = "jam1";
+            this.jam1.Size = new System.Drawing.Size(125, 27);
+            this.jam1.TabIndex = 11;
+            // 
+            // ts3
+            // 
+            this.ts3.Location = new System.Drawing.Point(221, 202);
+            this.ts3.Name = "ts3";
+            this.ts3.Size = new System.Drawing.Size(135, 27);
+            this.ts3.TabIndex = 10;
+            // 
+            // ts2
+            // 
+            this.ts2.Location = new System.Drawing.Point(221, 151);
+            this.ts2.Name = "ts2";
+            this.ts2.Size = new System.Drawing.Size(135, 27);
+            this.ts2.TabIndex = 9;
+            // 
+            // ts1
+            // 
+            this.ts1.Location = new System.Drawing.Point(221, 88);
+            this.ts1.Name = "ts1";
+            this.ts1.Size = new System.Drawing.Size(135, 27);
+            this.ts1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Jenis Kegiatan";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(242, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Terselesaikan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(441, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Jam";
+            // 
+            // Box3
+            // 
+            this.Box3.Location = new System.Drawing.Point(11, 202);
+            this.Box3.Name = "Box3";
+            this.Box3.Size = new System.Drawing.Size(161, 27);
+            this.Box3.TabIndex = 2;
+            // 
+            // Box2
+            // 
+            this.Box2.Location = new System.Drawing.Point(11, 151);
+            this.Box2.Name = "Box2";
+            this.Box2.Size = new System.Drawing.Size(161, 27);
+            this.Box2.TabIndex = 1;
+            // 
+            // Box1
+            // 
+            this.Box1.Location = new System.Drawing.Point(11, 88);
+            this.Box1.Name = "Box1";
+            this.Box1.Size = new System.Drawing.Size(161, 27);
+            this.Box1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(231, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "HISTORY HARIAN";
+            // 
+            // History_harian
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 570);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.keluar);
+            this.Controls.Add(this.Sabtu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Jumaat);
+            this.Controls.Add(this.Kamis);
+            this.Controls.Add(this.rabu);
+            this.Controls.Add(this.Selasa);
+            this.Controls.Add(this.Senin);
+            this.Name = "History_harian";
+            this.Text = "History Harian";
+            this.Load += new System.EventHandler(this.History_harian_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Senin;
+        private System.Windows.Forms.Button Selasa;
+        private System.Windows.Forms.Button rabu;
+        private System.Windows.Forms.Button Kamis;
+        private System.Windows.Forms.Button Jumaat;
+        private System.Windows.Forms.Button Sabtu;
+        private System.Windows.Forms.Button keluar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Box3;
+        private System.Windows.Forms.TextBox Box2;
+        private System.Windows.Forms.TextBox Box1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Box4;
+        private System.Windows.Forms.TextBox ts4;
+        private System.Windows.Forms.TextBox jam4;
+        private System.Windows.Forms.TextBox jam3;
+        private System.Windows.Forms.TextBox jam2;
+        private System.Windows.Forms.TextBox jam1;
+        private System.Windows.Forms.TextBox ts3;
+        private System.Windows.Forms.TextBox ts2;
+        private System.Windows.Forms.TextBox ts1;
+    }
+}
+
